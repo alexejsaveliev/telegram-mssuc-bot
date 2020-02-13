e60408ecd35c78894b3ad893084a28eccee5f042
Aleksey Savelyev <Aleksey_Savelyev@epam.com>
2020-02-13 18:48:20 +0100

    added spotify API

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,13 @@
-import Telegraf, {ContextMessageUpdate, Context} from 'telegraf';
+import Telegraf, { ContextMessageUpdate, Context } from 'telegraf';
 import axios from 'axios';
-import { TELEGRAM_API_KEY }  from './utils/secrets';
+import querystring from 'querystring';
+
+import logger from './utils/logger'
+import { TELEGRAM_API_KEY, SPOTIFY_CLIENT_ID, SPOTIFY_REFRESH_TOKEN, SPOTIFY_SECRET } from './utils/secrets';
 
 const allowedServices = [
-    {id: 'open.spotify.com', name: 'Spotify'},
-    {id: 'music.apple.com', name: 'Apple Music'}
+    { id: 'open.spotify.com', name: 'Spotify' },
+    { id: 'music.apple.com', name: 'Apple Music' }
 ];
 
 const apiKey = TELEGRAM_API_KEY || '';
@@ -12,27 +15,26 @@ const apiKey = TELEGRAM_API_KEY || '';
 const bot = new Telegraf(apiKey);
 bot.start((ctx: ContextMessageUpdate) => ctx.reply('Welcome'));
 bot.help((ctx: ContextMessageUpdate) => ctx.reply('Send me a sticker'));
-bot.on('text', (ctx: Context) => {
+bot.on('text', async (ctx: ContextMessageUpdate) => {
     const message = ctx.update.message?.text || '';
     const messageIncludeLink = allowedServices.some(({ id }) => message.includes(id));
 
     if (messageIncludeLink) {
         console.log(message)
-        getSpotifyDataByURL(message);
+        const newLink = await getSpotifyURL(message);
+        if (newLink) {
+            ctx.reply(newLink);
+        }
     }
 });
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.command('hipster', Telegraf.reply('λ'))
 
-async function getSpotifyDataByURL(url: string) {
+async function getSpotifyURL(url: string) {
     // console.log(url)
     let baseUrl = 'https://itunes.apple.com/';
-    let searchType = 'album'; 
+    let searchType = 'album';
     if (!url.includes('/album')) {
-        return
+        return ''
     }
 
     if (url.includes('?i=')) { //song
@@ -41,7 +43,7 @@ async function getSpotifyDataByURL(url: string) {
 
     let srtArr = url.match(/\w+$/g) || [];
     if (!srtArr.length) {
-        return
+        return ''
     }
     const searchUrl = `${baseUrl}lookup?id=${srtArr[0]}&entity=${searchType}`;
     console.log(searchUrl)
@@ -49,30 +51,64 @@ async function getSpotifyDataByURL(url: string) {
     try {
         const res = await axios.get(searchUrl);
         console.log(res.data);
-        console.log('next');
-        const link = await getSpotifyLink();
-        console.log(link)
+        if (res.data?.resultCount) {
+            const {artistName, collectionType, collectionName} = res.data.results[0];
+            const spotifyData = await getSpotifyData(searchType, cleanString(artistName + ' ' + collectionName));
+            
+            if (spotifyData.albums?.total) {
+                return spotifyData.albums.items[0].external_urls.spotify;
+            }
+        }
     } catch (error) {
-        console.log(error)
+        throw error;
     }
 
 }
 
-async function getSpotifyLink() {
+async function getSpotifyData(type: string, queryString: string) {
+
     try {
+        const accessToken = await getSpotifyAccessToken();
+
         const res = await axios({
             method: 'get',
-            url: 'https://api.spotify.com/v1/search?q=Iss004&type=album',
+            url: `https://api.spotify.com/v1/search?q=${encodeURI(queryString)}&type=${type}`,
             responseType: 'json',
-            headers: { 'Authorization': 'Bearer BQAbxfiaTTrDEAiAGLdEQ767odXh-6zKVxIwr4o5rBh7THJwU0xCuEevsXZQaFFpVkdbObiL8aeizqHLdIu3q0OyY9LaljP1-BfXuIP2g05sd23TM-xJ5hW8tup0yvCoi56j4kDMQqU7-w',
+            headers: { 'Authorization': 'Bearer ' + accessToken,
                         'Accept': 'application/json',
                         'Content-Type': 'application/json'}
           });
         return res.data;
     } catch (error) {
         throw error;
-        
     }
+}
+
+async function getSpotifyAccessToken() {
+    try {
+        const res = await axios({
+            url: 'https://accounts.spotify.com/api/token',
+            method: 'POST',
+            headers: { 
+                'Authorization': 'Basic ' + (Buffer.from(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_SECRET).toString('base64')),
+            },
+            responseType: 'json',
+            params: {
+                grant_type: 'refresh_token',
+                refresh_token: SPOTIFY_REFRESH_TOKEN
+            }
+        })
+
+        if (res.status === 200) {
+            return res.data.access_token;
+        }
+    } catch (error) {
+        logger.error("Error while getting access token.");
+    }
+}
+
+function cleanString(str:string) {
+    return str.replace(' - EP', '');
 }
 
 bot.launch();
--- app/utils/secrets.ts
+++ app/utils/secrets.ts
@@ -13,6 +13,9 @@ const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
 export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
 export const TELEGRAM_API_KEY = process.env["TELEGRAM_API_KEY"];
+export const SPOTIFY_CLIENT_ID = process.env["SPOTIFY_CLIENT_ID"];
+export const SPOTIFY_SECRET = process.env["SPOTIFY_SECRET"];
+export const SPOTIFY_REFRESH_TOKEN = process.env["SPOTIFY_REFRESH_TOKEN"];
 
 if (!TELEGRAM_API_KEY) {
     if (prod) {
