d1875d0d7a937f1ffbd88e43890ab4813f0c4897
Alex Savelyev <alexejsaveliev@gmail.com>
2020-02-21 16:30:07 +0100

    logger fixed for heroku

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -3,15 +3,15 @@ import winston from "winston";
 const { combine, timestamp, label, printf } = winston.format;
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
-    return `${timestamp} [${label}] ${level}: ${message}`;
+    return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
 });
 
 const options: winston.LoggerOptions = {
-    format: combine(
-        label({ label: process.env.NODE_ENV }),
-        timestamp(),
-        myFormat
-    ),
+    // format: combine(
+    //     label({ label: process.env.NODE_ENV }),
+    //     timestamp(),
+    //     myFormat
+    // ),
     transports: [
         new winston.transports.Console({
             level: process.env.NODE_ENV === "production" ? "error" : "debug"
@@ -21,6 +21,7 @@ const options: winston.LoggerOptions = {
 };
 
 const logger = winston.createLogger(options);
+logger.info("Logging initialized");
 
 if (process.env.NODE_ENV !== "production") {
     logger.debug("Logging initialized at debug level");
